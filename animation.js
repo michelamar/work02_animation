e8f1b813a0919b16ba44f42dbd578752d2a4ea22
Michela <michela@John-Marchini.local>
2018-02-07 23:54:33 -0500

    working

--- animation.js
+++ animation.js
@@ -2,19 +2,25 @@ var c = document.getElementById("yes_we_canvas");
 var ctx = c.getContext("2d");
 
 var b1 = document.getElementById("clear");
-var b1 = document.getElementById("stop");
+var b2 = document.getElementById("stop");
 
 var requestID;
-var radius = 0
 
 var clear = function(){
     ctx.beginPath();
     ctx.clearRect(0, 0, 500, 500);
     ctx.stroke();
+    window.cancelAnimationFrame(requestID);
+};
+
+var stop = function(){
+    window.cancelAnimationFrame(requestID);
 };
 
 var animate = function(){
     window.cancelAnimationFrame(requestID);
+    var radius = 0
+    var bigger = true;
     var draw = function(){
 	clear();
 	ctx.fillStyle = "lightsteelblue";
@@ -22,14 +28,21 @@ var animate = function(){
 	ctx.arc(c.width/2, c.height/2, radius, 0, 2*Math.PI);
 	ctx.fill();
 	ctx.stroke();
-	radius++;
+	if (bigger){
+	    radius++;
+	}
+	else{
+	    radius--;
+	}
+	if (radius <= 0 || radius >= c.height/2){
+	    bigger = !bigger;
+	}
 	requestID = window.requestAnimationFrame(draw);
-	console.log(requestID);
     };
     draw();
 };
 
 
 b1.addEventListener("click", clear);
-b1.addEventListener("click", clear);
+b2.addEventListener("click", stop);
 c.addEventListener("click", animate);
